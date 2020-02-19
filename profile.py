aae1fdaf4ad8715cec9b67b5de4e3ee1989b24ce
Yuhao Zhang <yhzhang1994@gmail.com>
2020-02-19 09:55:58 -0800

    added profile.py

---
+++ profile.py
@@ -0,0 +1,19 @@
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+# Add a raw PC to the request.
+node = request.RawPC("node")
+
+# Install and execute a script that is contained in the repository.
+node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
