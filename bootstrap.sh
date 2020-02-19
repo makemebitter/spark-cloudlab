0b44403e01dd3ac5c68547b1dfc45a68ce558ea2
Yuhao Zhang <yhzhang1994@gmail.com>
2020-02-19 13:58:29 -0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 duty=${1}
+JUPYTER_PASSWORD=${2:-"root"}
 set -e
 sudo apt-get update;
-sudo apt-get install -y openjdk-11-jre-headless scala openssh-server openssh-client syslinux-utils python3-pip;
+sudo apt-get install -y openjdk-11-jre-headless scala openssh-server openssh-client syslinux-utils python3-pip socat;
 wget https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz;
 tar xvf spark-2.4.4-bin-hadoop2.7.tgz;
 sudo mv spark-2.4.4-bin-hadoop2.7 /usr/local/spark;
-echo export PATH="$PATH:/usr/local/spark/bin" >> ~/.bashrc;
-echo export SPARK_HOME="$PATH:/usr/local/spark" >> ~/.bashrc;
-source ~/.bashrc;
+echo export PATH="$PATH:/usr/local/spark/bin" > ~/._bashrc;
+echo export SPARK_HOME="$PATH:/usr/local/spark" >> ~/._bashrc;
 sudo cp /usr/local/spark/conf/spark-env.sh.template /usr/local/spark/conf/spark-env.sh;
 sudo cp /usr/local/spark/conf/slaves.template /usr/local/spark/conf/slaves;
 
@@ -18,9 +18,18 @@ ips=($(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}'))
 for ip in "${ips[@]}"
 do
     if [[ $ip == *"10."* ]]; then
-        echo export LOCAL_IP=$ip >> ~/.bashrc;  
+        echo export LOCAL_IP=$ip >> ~/._bashrc;
+        LOCAL_IP=$ip
     fi
 done
+
+# sudo /usr/local/bin/jupyter contrib nbextension install --system ;
+# sudo /usr/local/bin/jupyter nbextensions_configurator enable --system ;
+# sudo /usr/local/bin/jupyter nbextension enable code_prettify/code_prettify --system ;
+# sudo /usr/local/bin/jupyter nbextension enable execute_time/ExecuteTime --system ;
+# sudo /usr/local/bin/jupyter nbextension enable collapsible_headings/main --system ;
+# sudo /usr/local/bin/jupyter nbextension enable freeze/main --system ;
+# sudo /usr/local/bin/jupyter nbextension enable spellchecker/main --system ;
 
 master_ip=$(gethostip -d master);
 echo "export SPARK_MASTER_HOST=$master_ip" | sudo tee -a /usr/local/spark/conf/spark-env.sh;
@@ -29,13 +38,20 @@ echo "export PYSPARK_PYTHON=python3.6" | sudo tee -a /usr/local/spark/conf/spark
 
 # echo "worker-0" | sudo tee /usr/local/spark/conf/slaves;
 
-cp ~/.bashrc /local/.bashrc
+cp ~/._bashrc /local/.bashrc
 
 if [ "$duty" = "m" ]; then
 	sudo bash /usr/local/spark/sbin/start-master.sh
+	sudo nohup socat TCP-LISTEN:8081,fork TCP:127.0.0.1:8080 > /dev/null 2>&1 &
+	sudo nohup socat TCP-LISTEN:4041,fork TCP:127.0.0.1:4040 > /dev/null 2>&1 &
+
 elif [ "$duty" = "s" ]; then
 	sudo bash /usr/local/spark/sbin/start-slave.sh $master_ip:7077
+	sudo nohup socat TCP-LISTEN:8082,fork TCP:127.0.0.1:8081 > /dev/null 2>&1 &	
 fi
+
+
+
 
 
 
--- profile.py
+++ profile.py
@@ -22,14 +22,16 @@ request = pc.makeRequestRSpec()
 node_worker_0 = request.RawPC('worker-0')
 node_worker_0.routable_control_ip = True
 node_worker_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
-node_worker_0.addService(pg.Execute('/bin/sh','sudo git clone https://github.com/makemebitter/spark-cloudlab.git /local/setup; sudo -H bash /local/setup/bootstrap.sh s > /local/logs/setup.log'))
+node_worker_0.addService(pg.Execute(
+    '/bin/sh', 'sudo git clone https://github.com/makemebitter/spark-cloudlab.git /local/setup; sudo -H bash /local/setup/bootstrap.sh s > /local/logs/setup.log'))
 iface0 = node_worker_0.addInterface('eth1')
 
 # Node master
 node_master = request.RawPC('master')
 node_master.routable_control_ip = True
 node_master.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
-node_master.addService(pg.Execute('/bin/sh','sudo git clone https://github.com/makemebitter/spark-cloudlab.git /local/setup; sudo -H bash /local/setup/bootstrap.sh m > /local/logs/setup.log'))
+node_master.addService(pg.Execute(
+    '/bin/sh', 'sudo git clone https://github.com/makemebitter/spark-cloudlab.git /local/setup; sudo -H bash /local/setup/bootstrap.sh m > /local/logs/setup.log'))
 iface1 = node_master.addInterface('eth1')
 
 # Link link-0
